6c07c2798e2d72ff6b0701b9fc9ee27e4407563f
Carlo De Prosperis <carlodep@outlook.it>
2024-11-03 15:10:45 +0100

    User model makmigrations.

--- app/core/models.py
+++ app/core/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def crete_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -27,5 +27,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,10 +8,10 @@ from django.contrib.auth import get_user_model
 class ModelTest(TestCase):
     """Test models."""
 
-    def test_create_user_with_email_succesfull(self):
-        """Test creating a user with an email is succesfull."""
-        email = "test@sample.com"
-        password = "testpass123"
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@sample.com'
+        password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
