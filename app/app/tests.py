b62cbe7324c73ad2204f851b0f2c1e660f6cbdb8
Carlo De Prosperis <carlodep@outlook.it>
2024-11-03 12:45:16 +0100

    Fixed test anomalies.

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,9 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_number(self):
         """Test subtraction x from y."""
-        res =calc.subtract(10, 15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
