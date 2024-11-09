dbf5da223c32d67b9af446236a358b8c800a29c6
Carlo De Prosperis <carlodep@outlook.it>
2024-11-09 16:15:42 +0100

    Lezione 67 corretta.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,5 @@
 """
-Test for models.
+Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -22,22 +22,23 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
-        sample_email = [
+        sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2@example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
-        for email, expected in sample_email:
-            user = get_user_model().objects.create_user(email, 'Sample123')
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
-        """Test tath create a user without an email raises a ValueError."""
+        """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
+        """Test creating superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
             'test123',
